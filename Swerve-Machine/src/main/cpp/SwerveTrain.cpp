8fa59c1c27b8318e70fd87f5a1d93427de04c29c
pilchd <nic.pilcher@gmail.com>
2020-01-13 19:25:48 -0600

    Cleaned and removed unworking drive method

--- Swerve-Machine/src/main/cpp/SwerveTrain.cpp
+++ Swerve-Machine/src/main/cpp/SwerveTrain.cpp
@@ -5,38 +5,15 @@
 void SwerveTrain::driveController(frc::XboxController &controller) {
 
     //Get the current REV rotation position of the swerves
-    const double frPos = m_frontRight->getSwervePosition();
-    const double flPos = m_frontLeft->getSwervePosition();
-    const double rlPos = m_rearLeft->getSwervePosition();
-    const double rrPos = m_rearRight->getSwervePosition();
+    const double currentFrPos = m_frontRight->getSwervePosition();
     //Get the current amount of rotations we need to set from center
-    const double rotateToPosition = getControllerRotationsFromCenter(controller.GetX(frc::GenericHID::kLeftHand), controller.GetY(frc::GenericHID::kLeftHand));
+    const double controllerREVRotationsFromCenter = getControllerREVRotationsFromCenter(controller.GetX(frc::GenericHID::kLeftHand), controller.GetY(frc::GenericHID::kLeftHand));
     //Get the current magnitude of the controller's joystick
     const double controllerMagnitude = getAbsoluteControllerMagnitude(controller.GetX(frc::GenericHID::kLeftHand), controller.GetY(frc::GenericHID::kLeftHand));
 
-    if (!(m_frontRightSwerveZeroPosition + rotateToPosition < m_frontRightSwerveZeroPosition + R_nicsConstant)) {
+    //If the control stick is within deadzone, return to the zero position
+    if (abs(controller.GetX(frc::GenericHID::kLeftHand)) < R_playerOneControllerDeadzone && abs(controller.GetY(frc::GenericHID::kLeftHand)) < R_playerOneControllerDeadzone) {
 
-        m_frontRightSwerveZeroPosition += R_nicsConstant;
-        m_frontLeftSwerveZeroPosition += R_nicsConstant;
-        m_rearLeftSwerveZeroPosition += R_nicsConstant;
-        m_rearRightSwerveZeroPosition += R_nicsConstant;
         assumeSwerveZeroPosition();
     }
-    else if (!(m_frontRightSwerveZeroPosition - rotateToPosition > m_frontRightSwerveZeroPosition - R_nicsConstant)) {
-
-        m_frontRightSwerveZeroPosition -= R_nicsConstant;
-        m_frontLeftSwerveZeroPosition -= R_nicsConstant;
-        m_rearLeftSwerveZeroPosition -= R_nicsConstant;
-        m_rearRightSwerveZeroPosition -= R_nicsConstant;
-        assumeSwerveZeroPosition();
-    }
-    else {
-
-        m_frontRight->assumeSwervePosition(frPos + rotateToPosition);
-        m_frontLeft->assumeSwervePosition(flPos + rotateToPosition);
-        m_rearLeft->assumeSwervePosition(rlPos + rotateToPosition);
-        m_rearRight->assumeSwervePosition(rrPos + rotateToPosition);
-    }
-
-    setDriveSpeed(controllerMagnitude);
 }
