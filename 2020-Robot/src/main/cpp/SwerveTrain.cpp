34ac2c08e067afa40ca271eb1eda082fb7ea86dc
milesku01 <mileskuhn5@hotmail.com>
2020-01-19 16:14:54 -0600

    Added turning to SwerveTrain

--- 2020-Robot/src/main/cpp/SwerveTrain.cpp
+++ 2020-Robot/src/main/cpp/SwerveTrain.cpp
@@ -5,14 +5,25 @@
 void SwerveTrain::driveController(frc::XboxController *controller) {
 
     const double controllerREVRotationsFromCenter = getControllerREVRotationsFromCenter(controller);
+    const double controllerTurningMagnitude = controller->GetX(frc::GenericHID::kRightHand); 
+
     const double controllerMagnitude = getControllerAbsoluteMagnitude(controller);
 
     //If the control stick is within deadzone, return to the zero position
-    if (abs(controller->GetX(frc::GenericHID::kLeftHand)) < R_playerOneControllerDeadzone && abs(controller->GetY(frc::GenericHID::kLeftHand)) < R_playerOneControllerDeadzone) {
+    if (controllerIsWithinDeadzone(controller)) {
         
         assumeNearestZeroPosition();
         setDriveSpeed(0);
+    } 
+    else if(abs(controllerTurningMagnitude) > R_playerOneControllerDeadzone) {
+
+        m_frontRight->assumeSwervePosition((1.0/8.0)*R_nicsConstant);
+        m_frontLeft->assumeSwervePosition((3.0/8.0)*R_nicsConstant);
+        m_rearLeft->assumeSwervePosition((5.0/8.0)*R_nicsConstant);
+        m_rearRight->assumeSwervePosition((7.0/8.0)*R_nicsConstant);
+        setDriveSpeed(controllerTurningMagnitude / 4.0);
     }
+
     else {
         
         m_frontRight->assumeSwervePosition(controllerREVRotationsFromCenter);
