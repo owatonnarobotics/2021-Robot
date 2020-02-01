cabd32752be124546221471f0de249d7a7f2a4d6
pilchd <nic.pilcher@gmail.com>
2020-02-01 09:54:03 -0600

    Two climb motors with enumerator

--- 2020-Robot/src/main/include/Climber.h
+++ 2020-Robot/src/main/include/Climber.h
@@ -1,34 +1,62 @@
 #pragma once;
 
-#include "rev/CANSparkMax.h"
 #include "frc/smartdashboard/SmartDashboard.h"
+
+#include "rev/CANSparkMax.h"
 
 class Climber {
 
     public:
-        Climber(const int &climbMotorCANID) {
+        Climber(const int &primaryMotorCANID, const int &secondaryMotorCANID) {
 
-            climbMotor = new rev::CANSparkMax(climbMotorCANID, rev::CANSparkMax::MotorType::kBrushless);
-            climbMotorEncoder = new rev::CANEncoder(climbMotor->GetEncoder());
-            climbMotor->SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
+            m_primaryMotor = new rev::CANSparkMax(primaryMotorCANID, rev::CANSparkMax::MotorType::kBrushless);
+            m_secondaryMotor = new rev::CANSparkMax(secondaryMotorCANID, rev::CANSparkMax::MotorType::kBrushless);
+
+            m_primaryMotorEncoder = new rev::CANEncoder(m_primaryMotor->GetEncoder());
+            m_secondaryMotorEncoder = new rev::CANEncoder(m_secondaryMotor->GetEncoder());
+
+            m_primaryMotor->SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
+            m_secondaryMotor->SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
         }
 
-        void setSpeed(const double &speedToSet) {
+        void setSpeed(const int &motor, const double &speedToSet = 0) {
 
-            climbMotor->Set(speedToSet);
+            if (motor == LiftMotor::kPrimary) {
+
+                m_primaryMotor->Set(speedToSet);
+            }
+            if (motor == LiftMotor::kSecondary) {
+
+                m_primaryMotor->Set(speedToSet);
+            }
+            if (motor == LiftMotor::kBoth) {
+
+                m_primaryMotor->Set(speedToSet);
+                m_secondaryMotor->Set(speedToSet);
+            }
         }
 
-        double getSpeed() {
+        double getSpeed(const int &motor) {
             
-            return climbMotor->Get();
+            if (motor == LiftMotor::kPrimary) {
+
+                return m_primaryMotorEncoder->GetVelocity();
+            }
+            if (motor == LiftMotor::kSecondary) {
+
+                return m_secondaryMotorEncoder->GetVelocity();
+            }
         }
 
-        double getRPM() {
+        enum LiftMotor {
 
-            return climbMotorEncoder->GetVelocity();
-        }
+            kPrimary, kSecondary, kBoth
+        };
 
     private:
-        rev::CANSparkMax *climbMotor;
-        rev::CANEncoder *climbMotorEncoder;
+        rev::CANSparkMax *m_primaryMotor;
+        rev::CANSparkMax *m_secondaryMotor;
+
+        rev::CANEncoder *m_primaryMotorEncoder;
+        rev::CANEncoder *m_secondaryMotorEncoder;
 };
