f28de83cd7ba97fda214d88eb2a87477fa3bfbe2
Weatherman142 <alexhamilton0403@gmail.com>
2021-03-30 17:44:51 -0500

    Added documentation to CameraLock and changed boolean.

--- 2021-Robot/src/main/include/auto/steps/CameraLock.h
+++ 2021-Robot/src/main/include/auto/steps/CameraLock.h
@@ -5,12 +5,11 @@
 #include "RobotMap.h"
 #include "auto/steps/AssumeRotationDegrees.h"
 #include "auto/steps/WaitSeconds.h"
-#include <opencv/cv.hpp>
 
 Vision cameraView;
 
 //Rotates Zion to attempt to find powercell.
-//Will need more documentation in future changes.
+//Will need more documentation in future revisions.
 
 class CameraLock : public AutoStep {
 
@@ -31,7 +30,7 @@ class CameraLock : public AutoStep {
 
         void Init(){
             
-            //Initializes the image and how many degrees per turn.
+            //Gets the first image and how many degrees per individual rotation.
             m_image = cameraView.imageCapturer();
             degreesPerTurn = 30;
         }
@@ -42,23 +41,24 @@ class CameraLock : public AutoStep {
             int turnsMade = 0;
             
             //Turns Zion in 30 degree increments until it sees a circle larger than 4 pixels in radius
-            //or has reached a full circle of rotation. Prevents non-stop turning, if none is seen.
+            //or has reached a full circle of rotation. Prevents non-stop turning, if none are seen.
             while(largestVector[2] < 4 && turnsMade < (360 / degreesPerTurn)) {
 
-                turnsMade++;
                 AssumeRotationDegrees(*m_zion, *m_limelight, *m_navX, degreesPerTurn);
                 WaitSeconds(0.75);
+                turnsMade++;
 
                 m_image = cameraView.imageCapturer();
                 largestVector = cameraView.largestVectorByImage(m_image);
             }
             
+            //Once power cell is in view, rotate to line up using x coordinates of it.
             m_image = cameraView.imageCapturer();
             double degreesToTurnRound = cameraView.degreesToTurn(m_image);
 
             AssumeRotationDegrees(*m_zion, *m_limelight, *m_navX, degreesToTurnRound);
 
-            return false;
+            return true;
         }
     
     private:
@@ -66,8 +66,7 @@ class CameraLock : public AutoStep {
         SwerveTrain* m_zion;
         Limelight* m_limelight;
         NavX* m_navX;
-        double degreesPerTurn;
-        
+        double degreesPerTurn;   
 };
 
 #endif
